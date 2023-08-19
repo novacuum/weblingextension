83890add2287172cbeb9c522c32a7d07b7ec6ee5
simon-at-fugu <simon.hiller@fugu.ch>
2023-08-20 01:59:42 +0200

    fix infinite re-renders problem

--- frontend/views/member/MemberView.tsx
+++ frontend/views/member/MemberView.tsx
@@ -2,12 +2,15 @@ import { MemberEndpoint } from 'Frontend/generated/endpoints';
 import { Grid } from '@hilla/react-components/Grid.js';
 import { Button } from "@hilla/react-components/Button.js";
 import { GridSortColumn } from '@hilla/react-components/GridSortColumn.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Member from 'Frontend/generated/ch/slackattack/webling/repo/Member';
 
 export default function MemberView() {
   const [members, setMembers] = useState<Member[]>([]);
-  MemberEndpoint.findAll().then(members=>setMembers(members));
+
+  useEffect(() => {
+    MemberEndpoint.findAll().then(members=>setMembers(members));
+  }, []);
 
   return (
     <>
